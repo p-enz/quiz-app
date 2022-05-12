327aa826b89b27a3d8f330ca4d7083ce2f7943f5
Philipp Enz <programming@phenz.de>
2022-05-12 23:16:49 +0200

    implemt bookmarkicon function for first card

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-/*
-    Aufg 22_05_09 Nr1 
-*/
-const bookmark = document.querySelector('.card__logo');
-bookmark.addEventListener('click', changeBookmarkIcon);
+const notSetBookmark = document.querySelector('[js-data="notBookmarked"]');
+const setBookmark = document.querySelector('[js-data="bookmarked"]');
 
-function changeBookmarkIcon() {
-  bookmark.classList.toggle('card__logo--isMarked');
-  console.log('inner Function');
+notSetBookmark.addEventListener('click', setIconAsBookmarked);
+setBookmark.addEventListener('click', setIconAsBookmarked);
+
+/*TO DO: ASK COACHES about forEach with 2 different items, depending on each other, 
+thoughts i could simply toogle in the class card--hide and "fill" the svg instead of really 
+filling it i use 2 different svgs with different selectors single card worked,
+but i dont get the method to apply on all:
+
+const notSetBookmarks = document.querySelectorAll('[js-data="notBookmarked"]');
+const setBookmarks = document.querySelectorAll('[js-data="bookmarked"]');
+
+notSetBookmarks.forEach(notSetBookmark => {
+  notSetBookmark.addEventListener('click', setIconAsBookmarked);
+});
+*/
+function setIconAsBookmarked() {
+  notSetBookmark.classList.toggle('card--hide');
+  setBookmark.classList.toggle('card--hide');
 }
 
 /*
     Aufg 22_05_09 Nr2
 */
+
+/*
 const showAnswerBtn = document.querySelector('.card__hideAnswer');
 showAnswerBtn.addEventListener('click', showAnswer);
 
@@ -25,3 +39,4 @@ function showAnswer() {
 
   //showAnswerBtn.classList.toggle(".card__showAnswer");
 }
+*/
