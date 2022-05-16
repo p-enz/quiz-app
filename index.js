c2b2e7f1f16a1c4342e6ef83849701b2986f5946
Philipp Enz <programming@phenz.de>
2022-05-16 12:54:53 +0200

    add function to change header content

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ bookmarkPaths.forEach(path => toggleBookmarkIcon(path));
 const questionCards = document.querySelectorAll('.card');
 questionCards.forEach(questionCard => showAnswerOnClick(questionCard));
 
-/* For SinglePageApplication */
-//const pages = document.querySelectorAll('[js-data="page"]');
+/* 
+For SinglePageApplication 
+TODO: Refactor a bit, maybe add another foreach for buttons?
+*/
 const homeButton = document.querySelector('[js-data="toIndexPage"]');
 const bookmarkButton = document.querySelector('[js-data="toBookmarkPage"]');
 const createButton = document.querySelector('[js-data="toCreatePage"]');
@@ -17,14 +19,38 @@ const profileButton = document.querySelector('[js-data="toProfilePage"]');
 
 homeButton.addEventListener('click', () => {
   showChosenPage('indexPage');
+  changeHeadline('Quiz-App');
 });
 bookmarkButton.addEventListener('click', () => {
   showChosenPage('bookmarkPage');
+  changeHeadline('Bookmarks');
 });
 createButton.addEventListener('click', () => {
   showChosenPage('createPage');
+  changeHeadline('Create New Question');
 });
 profileButton.addEventListener('click', () => {
   showChosenPage('profilePage');
+  changeHeadline('Your Profile');
 });
 /*----------------------------------------------*/
+
+const inQuestion = document.querySelector('#inQuestion');
+const inAnwser = document.querySelector('#inAnswer');
+countInputCharacters(inQuestion);
+countInputCharacters(inAnwser);
+
+//querySelector for charCount needs to be fixed
+function countInputCharacters(element) {
+  element.addEventListener('input', () => {
+    const charCount = document.querySelector('.create__questionCharCount');
+    charCount.textContent = `Characters: ${element.value.length}`;
+  });
+}
+
+/* change Headline  it works but is not really nice function*/
+
+function changeHeadline(headlineContent) {
+  const headline = document.querySelector('h1');
+  headline.textContent = headlineContent;
+}
--- javascripts/singlePageApplication/showChosenPage.js
+++ javascripts/singlePageApplication/showChosenPage.js
@@ -1,7 +1,6 @@
 function showChosenPage(className) {
   hideAllPages();
   const pageToShow = document.querySelector('.' + className);
-  console.log(document.querySelector('.' + className));
   pageToShow.classList.remove('page--hide');
 }
 
